41341e86fb00cd1103475152e12fa403c2b09f85
Rosa Huisman <rosa@macbook-pro-de-rosa.home>
2022-04-30 13:32:15 +0200

    database

--- app/database.js
+++ app/database.js
@@ -1,7 +1,13 @@
 const { Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(process.env.PG_URL, {
-    
+console.log(process.env.PG_URL)
+
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize(process.env.PG_URL,{
+    define: {
+      timestamps: false
+    },
 });
 
 module.exports = sequelize;

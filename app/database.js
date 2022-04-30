8a75ee054036d84e59b1b571daafe716591ab90b
Rosa Huisman <rosa@macbook-pro-de-rosa.home>
2022-04-30 14:33:31 +0200

    sequelize

--- app/database.js
+++ app/database.js
@@ -3,8 +3,6 @@ const { Sequelize} = require('sequelize');
 console.log(process.env.PG_URL)
 
 const sequelize = new Sequelize(process.env.PG_URL,{
-    host: 'mysql',
-    dialect: 'mysql',
     define: {
       timestamps: false
     },
